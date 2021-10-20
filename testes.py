7db0ed78b4c3e9c95d3103550dd779360362b189
Bruno <brunosssss@gmail.com>
2021-10-20 15:05:47 -0300

    ultima atualizacao

---
+++ testes.py
@@ -0,0 +1,16 @@
+n1 = 10
+multiplo_3 = 3
+multiplo_5 = 5
+contador3 = 0
+contador5 = 0
+
+while (n1 > multiplo_5 ):
+    contador5 =+ 1
+    multiplo_5 =+multiplo_5
+
+while (n1 > multiplo_3):
+    contador3 =+ 1
+    multiplo_3 =+ multiplo_3
+
+
+print(f" O numero {n1} tem ")
